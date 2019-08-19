21309888ed625149b32ff9f656ff4b6c0993e262
Connor Caillet <s198063@students.aie.edu>
2019-08-19 16:44:12 -0500

    Add project files.

---
+++ Looping/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Looping
+{
+    class Program
+    {
+
+        
+         
+        static void Main(string[] args)
+        {
+            int count = 0;
+
+            while (count < 3) ;
+            count++;
+            Console.WriteLine("the loop has run " + count + " times. ");
+        }
+        Console.Readkey();
+    }
+}
