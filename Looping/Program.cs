54ac7d674ac3f4005edcb357f811eaa347c4900a
louis-e <44675238+louis-e@users.noreply.github.com>
2019-08-20 23:53:48 +0200

    Updated Program.cs
    It works now

--- Looping/Program.cs
+++ Looping/Program.cs
@@ -8,17 +8,17 @@ namespace Looping
 {
     class Program
     {
-
-        
-         
         static void Main(string[] args)
         {
             int count = 0;
 
-            while (count < 3) ;
-            count++;
-            Console.WriteLine("the loop has run " + count + " times. ");
+            while (count < 3)
+            {
+                count++;
+                Console.WriteLine("The loop has run " + count + " times.");
+            }
+
+            Console.ReadKey();
         }
-        Console.Readkey();
     }
 }
